8382a09fc3506e81bc7713cf13ba99d761d47d15
Ni Tianhua <nitianhua@qq.com>
2018-05-24 14:47:32 +0800

    Initial commit

---
+++ app/Http/Controllers/WeChat/WeChatController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nitia
+ * Date: 2018/5/24 0024
+ * Time: 14:13
+ */
+
+namespace App\Http\Controllers\WeChat;
+
+
+use App\Http\Controllers\Controller;
+use EasyWeChat;
+
+class WeChatController extends Controller
+{
+    public $app;
+
+    public function __construct()
+    {
+        $this->app = EasyWeChat::officialAccount();
+    }
+
+    public function serve()
+    {
+        $this->app->server->push(function($message){
+            return "Here is the card demo！";
+        });
+
+        return $this->app->server->serve();
+
+    }
+
+    public function test()
+    {
+        return "this is test";
+    }
+
+}
