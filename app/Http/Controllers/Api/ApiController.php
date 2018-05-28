f7e127d7a662709290912ba41acd2d3288cb1d84
Ni Tianhua <nitianhua@qq.com>
2018-05-28 17:36:39 +0800

    asd

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Helpers\Api\ApiResponse1;
+use App\Helpers\Api\ApiResponse;
 use App\Http\Controllers\Controller;
 
 class ApiController extends Controller
