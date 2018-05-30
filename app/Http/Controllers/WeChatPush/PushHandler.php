75c72255d62bce067a6283999761b3928791f852
Ni Tianhua <nitianhua@qq.com>
2018-05-30 15:59:52 +0800

    asd

--- app/Http/Controllers/WeChatPush/PushHandler.php
+++ app/Http/Controllers/WeChatPush/PushHandler.php
@@ -24,13 +24,14 @@ class PushHandler extends WeChatController implements EventHandlerInterface
     public function handle($payload = null)
     {
 //        $MsgType = $this->push_message['MsgType'];
-        $MsgType = $payload['MsgType'];
-        $className = ucfirst(strtolower($MsgType))."MessageHandler";
-        if(class_exists($className)){
-            $message_handler = new $className();
-            return $message_handler->reply($payload);
-        }else{
-            return '啊啊啊';
-        };
+//        $MsgType = $payload['MsgType'];
+//        $className = ucfirst(strtolower($MsgType))."MessageHandler";
+//        if(class_exists($className)){
+//            $message_handler = new $className();
+//            return $message_handler->reply($payload);
+//        }else{
+//            return '啊啊啊';
+//        };
+        return "啊哈哈";
     }
 }
